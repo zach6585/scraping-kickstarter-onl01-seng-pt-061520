08c5bb34680c0dd095becc05a71ad4bac187c4fe
zachary mines <zmines@wesleyan.edu>
2020-07-24 22:11:27 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,7 +13,7 @@ def create_project_hash
     :location =>  
   project.css("span.location-name").text, 
   :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-end 
+  end 
   projects 
 end
 
