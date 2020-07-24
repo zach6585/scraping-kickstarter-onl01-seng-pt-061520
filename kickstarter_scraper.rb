6a13c2ded6184cc12063630448ce8420c1a64f31
zachary mines <zmines@wesleyan.edu>
2020-07-24 22:06:40 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,6 +11,7 @@ def create_project_hash
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   #description: project.css("p.bbcard_blub").text 
   #location: project.css("span.location-name").text
+  #project.css("ul.project-stats li.first.funded strong").text
 end
 
 create_project_hash
