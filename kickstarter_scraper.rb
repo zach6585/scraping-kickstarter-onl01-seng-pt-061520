c716f963f33ac552347efa71c0175f1f5f9db306
zachary mines <zmines@wesleyan.edu>
2020-07-24 22:08:13 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,10 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  projects = {} 
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {} 
+  end 
   #projects: 
   #project.css("li.project.grid_4")
   #title: project.css("h2.bbcard_name strong a").text
