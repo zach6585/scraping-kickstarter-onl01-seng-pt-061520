1bf13e41befa4ea49d850587f8a81d8c289456c8
zachary mines <zmines@wesleyan.edu>
2020-07-24 22:11:03 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,10 +7,11 @@ def create_project_hash
   projects = {} 
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {} 
-    image_link = project.css("div.project-thumbnail a img").attribute("src").value
-  #description: project.css("p.bbcard_blub").text 
-  #location: project.css("span.location-name").text
+    projects[title.to_sym] = {
+    :image_link => project.css("div.project-thumbnail a img").attribute("src").value, 
+    :description => project.css("p.bbcard_blub").text 
+    :location =>  
+  project.css("span.location-name").text
   #project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
   projects 
 end
